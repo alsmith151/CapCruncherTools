e01487a41474884fd9bfb4251a4aba7ca8d6bbcd
asmith <asmith151@outlook.com>
2022-03-17 18:43:01 +0000

    Updated bindings to use logging

--- capcruncher_tools/bindings.py
+++ capcruncher_tools/bindings.py
@@ -1,32 +1,64 @@
-
 import logging
 import pathlib
+import subprocess
+from joblib import Parallel, delayed
 from . import cct
 
 
-def fastq_deduplicate(infiles: tuple, output_prefix: str):
+def fastq_deduplicate(
+    infiles: tuple,
+    output_prefix: str,
+    output_compression: bool = False,
+    n_cores: int = 1,
+):
     """Deduplicates paired FASTQ files.
     Input files in format: fq1_1,fq2_1,fq3_1 fq1_2,fq2_2,fq3_2
     """
     fq_in = {
         str(ii): [r1, r2]
         for ii, (r1, r2) in enumerate(
-            zip(*[files.split(",") if "," in files else [files, ] for files in infiles])
+            zip(
+                *[
+                    files.split(",")
+                    if "," in files
+                    else [
+                        files,
+                    ]
+                    for files in infiles
+                ]
+            )
         )
     }
 
     fq_out = {
-        str(ii): [f"{output_prefix}{pathlib.Path(read).name}" for read in (r1, r2)]
+        str(ii): [
+            f"{output_prefix}{pathlib.Path(read).name.replace('.gz', '')}"
+            for read in (r1, r2)
+        ]
         for ii, (r1, r2) in enumerate(
-            zip(*[files.split(",") if "," in files else [files,] for files in infiles])
+            zip(
+                *[
+                    files.split(",")
+                    if "," in files
+                    else [
+                        files,
+                    ]
+                    for files in infiles
+                ]
+            )
         )
     }
 
     logging.debug(f"Input: {fq_in}")
     logging.debug(f"Output: {fq_out}")
 
-    deduplication_results = cct.deduplicate_fastq(fq_in, fq_out, True)
-    
+    deduplication_results = cct.deduplicate_fastq(
+        fq_in,  # Fastq input list
+        fq_out,  # Fastq output list
+        True,  # Shuffle files
+        output_compression,  # Compress output
+    )
+
     logging.debug(f"Deduplication results: {deduplication_results}")
 
     return deduplication_results
