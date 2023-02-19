a4918cae3089c4721c9b5a818e5ff8545e7821f7
asmith <asmith151@outlook.com>
2023-02-19 15:02:41 +0000

    Fixes import issues and removes redundant bindings

---
+++ python/capcruncher_tools/bindings.py
@@ -0,0 +1,18 @@
+# import logging
+# import pathlib
+# from typing import List, Tuple
+
+# from . import libcct
+
+# def fastq_deduplicate(
+#     infiles: Tuple[Tuple[str, str]],
+#     outfiles: Tuple[Tuple[str, str]],
+#     shuffle: bool = False,
+# ):
+
+#     stats = libcct.fastq_deduplicate(
+#         infiles,
+#         outfiles,
+#         shuffle)
+    
+#     return stats
