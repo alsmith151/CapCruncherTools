0bc712cf31033549f49d1e8888242a0c6fa94b2f
asmith <asmith151@outlook.com>
2023-02-25 23:10:14 +0000

    Adds digest genome to package

--- python/capcruncher_tools/cli.py
+++ python/capcruncher_tools/cli.py
@@ -6,7 +6,7 @@ import click
 import pandas as pd
 import tabulate
 
-from .capcruncher_tools import deduplicate
+from .capcruncher_tools import deduplicate, digest
 
 FORMAT = "%(levelname)s %(name)s %(asctime)-15s %(message)s"
 logging.basicConfig(format=FORMAT)
@@ -40,11 +40,12 @@ def cli():
     default=False,
 )
 def fastq_deduplicate(*args, **kwargs):
+    """Remove PCR duplicates from paired FASTQ files"""
 
     fq_input = list(zip(kwargs["fastq1"], kwargs["fastq2"]))
     fq_output = [
         (
-            kwargs["output_prefix"]+ os.path.basename(f1),
+            kwargs["output_prefix"] + os.path.basename(f1),
             kwargs["output_prefix"] + os.path.basename(f2),
         )
         for f1, f2 in fq_input
@@ -54,8 +55,8 @@ def fastq_deduplicate(*args, **kwargs):
     output_path.mkdir(parents=True, exist_ok=True)
 
     deduplication_results = deduplicate.fastq_deduplicate(
-        fq_input, # Infiles
-        fq_output, # Outfiles
+        fq_input,  # Infiles
+        fq_output,  # Outfiles
         kwargs["shuffle"],
     )
 
@@ -82,6 +83,46 @@ def fastq_deduplicate(*args, **kwargs):
     df_vis = df_vis[["stat_type", "stat"]]
     df_vis.columns = ["Stat Type", "Number of Reads"]
     print(tabulate.tabulate(df_vis, headers="keys", tablefmt="psql", showindex=False))
+
+
+@cli.command()
+@click.option("-i", "--input", help="Input fasta file", required=True)
+@click.option("-o", "--output", help="Output bed file", default="digested.bed")
+@click.option(
+    "-r", "--recognition-site", help="Enzyme recognition site", default="GATC"
+)
+@click.option(
+    "--min-slice-length",
+    help="Minimum slice length to keep",
+    default=18,
+)
+@click.option(
+    "--remove-recognition-site",
+    help="Remove recognition site from output",
+    is_flag=True,
+    default=False,
+)
+def digest_genome(*args, **kwargs):
+    """Digest genome with restriction enzyme and save to BED file"""
+
+    # fasta: String,
+    # restriction_site: String,
+    # output: String,
+    # remove_recognition_site: bool,
+    # min_slice_length: Option<usize>,
+
+    logging.info(f"Digesting genome using recognition site: {kwargs['recognition_site']}")
+    logging.info(f"{'Keeping' if not kwargs['remove_recognition_site'] else 'Removing'} recognition site")
+    logging.info(f"Minimum slice length: {kwargs['min_slice_length']}")
+    logging.info(f"Saving output to {kwargs['output']}")
+    
+    digest.digest_fasta(
+        kwargs["input"],
+        kwargs["recognition_site"],
+        kwargs["output"],
+        kwargs["remove_recognition_site"],
+        kwargs["min_slice_length"],
+    )
 
 
 if __name__ == "__main__":
--- src/genome_digest.rs
+++ src/genome_digest.rs
@@ -1,10 +1,175 @@
-// use std::prelude::rust_2021::*;
-// use log::{debug, info, warn};
-// use bio::io::fasta;
-// use bio::io::bed;
-// use bio::pattern_matching::bom::BOM;
-// use rayon::prelude::*;
+use std::fs::File;
+use std::sync::Arc;
 
+use bio::io::bed;
+use bio::io::fasta;
+use bio::pattern_matching::bom::BOM;
+use log::{debug, error, info, warn};
+use memchr::memmem;
+use rayon::prelude::*;
+use std::prelude::rust_2021::*;
+use std::rc::Rc;
+
+struct DigestedFastaEntry<'a> {
+    name: String,
+    sequence: &'a [u8],
+    restriction_site: &'a [u8],
+    remove_recognition_site: bool,
+    min_slice_size: Option<usize>,
+    slices: Vec<usize>,
+}
+
+impl<'a> DigestedFastaEntry<'a> {
+    fn new(
+        name: String,
+        sequence: &'a &[u8],
+        restriction_site: &'a [u8],
+        remove_recognition_site: bool,
+        min_slice_size: Option<usize>,
+    ) -> Self {
+        Self {
+            name,
+            sequence,
+            restriction_site,
+            remove_recognition_site,
+            min_slice_size,
+            slices: Vec::new(),
+        }
+    }
+
+    fn digest(&mut self) {
+        let matcher = BOM::new(self.restriction_site);
+        let mut slice_indexes: Vec<_> = matcher.find_all(self.sequence).collect();
+
+        // Add the start and end to slices
+        slice_indexes.insert(0, 0);
+        slice_indexes.push(self.sequence.len());
+
+        // Remove all slices that are smaller than the minimum slice size
+        if let Some(min_slice_size) = self.min_slice_size {
+            slice_indexes = slice_indexes
+                .into_iter()
+                .filter(|&x| x >= min_slice_size)
+                .collect();
+        }
+
+        self.slices = slice_indexes;
+    }
+
+    fn to_bed_records(&mut self) -> Vec<bed::Record> {
+        self.digest();
+        let mut bed_records = Vec::with_capacity(self.slices.len());
+
+        for (start, end) in self.slices.iter().zip(self.slices.iter().skip(1)) {
+            let mut bed_rec = bed::Record::new();
+            bed_rec.set_chrom(&self.name);
+
+            // Remove the recognition site from the slice if specified
+            if self.remove_recognition_site {
+                bed_rec.set_start((*start + self.restriction_site.len()) as u64);
+            } else {
+                bed_rec.set_start(*start as u64);
+            }
+
+            bed_rec.set_end(*end as u64);
+            bed_records.push(bed_rec);
+        }
+        bed_records
+    }
+}
+
+pub fn digest_fasta(
+    fasta: String,
+    restriction_site: String,
+    output: String,
+    remove_recognition_site: bool,
+    min_slice_length: Option<usize>,
+) -> Result<(), std::io::Error> {
+    let mut reader = fasta::Reader::from_file(fasta).expect("Error opening FASTA file");
+    let mut writer = bed::Writer::to_file(output).expect("Error opening BED output file");
+
+    let min_slice_length = min_slice_length.unwrap_or(0);
+    let restriction_site = restriction_site.as_bytes();
+
+    // Iterate over the records in parallel with rayon and digest each one
+    reader
+        .records()
+        .par_bridge()
+        .map(|record| {
+            let record = record.unwrap();
+            let seq = record.seq();
+
+            let mut digested_entry = DigestedFastaEntry::new(
+                record.id().to_string(),
+                &seq,
+                restriction_site,
+                remove_recognition_site,
+                Some(min_slice_length),
+            );
+
+            digested_entry.to_bed_records()
+        })
+        .collect::<Vec<Vec<bed::Record>>>()
+        .iter()
+        .for_each(|bed_records| {
+            for bed_rec in bed_records {
+                writer.write(bed_rec).unwrap();
+            }
+        });
+
+    Ok(())
+}
+
+// Test fasa digestion
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_digest_fasta() {
+        let fasta = "tests/chr14.fa".to_string();
+        let restriction_site = "GATC".to_string();
+        let output = "test.bed".to_string();
+        let remove_recognition_site = true;
+
+        digest_fasta(
+            fasta,
+            restriction_site,
+            output,
+            remove_recognition_site,
+            None,
+        )
+        .unwrap();
+    }
+}
+
+// pub fn digest_fasta_file(fasta_file: String, recognition_site: &[u8]) -> Result<(), std::io::Error>{
+
+//     let mut reader = fasta::Reader::from_file(fasta_file).expect("Error opening file");
+//     let mut writer = fasta::Writer::to_file("test.bed")?;
+
+//     let mut bed_records = Vec::new();
+
+//     // Iterate over the records in parallel with rayon and digest each one
+//     reader.records().par_bridge().for_each(|record| {
+//         let record = record.unwrap();
+//         let sequence = record.seq();
+//         let matcher = BOM::new(recognition_site);
+//         let mut slice_indexes: Vec<_> = matcher.find_all(sequence).collect();
+
+//         // Add the start and end to slices
+//         slice_indexes.insert(0, 0);
+//         slice_indexes.push(sequence.len());
+
+//         // Create a bed record for each slice
+//         for (i, slice) in slice_indexes.iter().enumerate(){
+//             let bed_rec = bed::Record::new();
+//             bed_records.push(bed_rec);
+//         }
+//     });
+
+//     Ok(())
+// }
 
 // fn digest_entry(entry: fasta::Record, recognition_site: &[u8]) -> Result<Vec<usize>, std::io::Error>{
 
@@ -37,10 +202,6 @@
 //     Ok(())
 // }
 
-
-
-
-
 // #[test]
 // fn test_digest_entry_with_rsite(){
 
@@ -54,9 +215,9 @@
 
 // #[test]
 // fn test_digest_fasta_file(){
-    
+
 //         let fasta_file = "tests/chr14.fa";
 //         let recognition_site = b"GATC";
 //         convert_fasta_to_digested_bed(fasta_file.to_string(), recognition_site).unwrap();
-    
+
 // }
