3d26279d5a9565cf5ef70980b724b2a7bc6da8e7
mtekman <20641402+mtekman@users.noreply.github.com>
2024-10-15 17:46:09 +0100

    Fix to mixed chromosome dtypes (#21)
    * Fix to mixed chromosome dtypes
    
    This is possibly more of an error in PyRanges than CapCruncherTools / CapCruncher, but it's easier to patch here
    
    * Added pyranges issue, and cast back to category

--- python/capcruncher_tools/api.py
+++ python/capcruncher_tools/api.py
@@ -217,6 +217,15 @@ def count_interactions(
             "Name": "name",
         }
     )
+    # Note that fragment_map does not contain headers, so PyRanges's
+    # read_bed function cannot cast the Chromosome column implicitly
+    # into a category type as it normally would.
+    ## See: https://github.com/pyranges/pyranges/issues/375
+    #
+    # For now, we have to do this ourselves: casting first to string
+    # and then category.
+    ## See: https://github.com/sims-lab/CapCruncher/issues/234
+    bins["chrom"] = bins["chrom"].astype("string").astype("category")
     bins_ref = ray.put(bins)
 
     # Fix reporters path
